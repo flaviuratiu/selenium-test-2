4dd589d36d6ff0776ddd07f1ea550fd248995ba3
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-02-02 11:28:10 +0200

    cssSelector and assertions

--- src/test/java/org/fasttrackit/cart/CartTest.java
+++ src/test/java/org/fasttrackit/cart/CartTest.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CartTest {
 
@@ -21,6 +25,14 @@ public class CartTest {
         // xpath concatenated here just to be displayed on two rows and be visible all at once
         driver.findElement(By.xpath("//div[@class='product-info' and ./descendant::*[text()" +
                 "='Herald Glass Vase']]//button[contains(@class, 'btn-cart')]")).click();
+
+        WebElement successMessageContainer = driver.findElement(By.cssSelector(".success-msg"));
+
+        assertThat("Product not added to cart.",
+                successMessageContainer.getText(),
+                containsString("Herald Glass Vase"));
+
+        // todo: assert that product is present in cart
     }
 
 }
--- src/test/java/org/fasttrackit/search/SimpleSearchTest.java
+++ src/test/java/org/fasttrackit/search/SimpleSearchTest.java
@@ -4,19 +4,39 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class SimpleSearchTest {
 
     @Test
-    public void SimpleSearchWithOneKeyword() {
+    public void simpleSearchWithOneKeyword() {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\webdrivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://fasttrackit.org/selenium-test/");
 
-        driver.findElement(By.id("search")).sendKeys("vase" + Keys.ENTER);
+        String keyword = "vase";
+        driver.findElement(By.id("search")).sendKeys(keyword + Keys.ENTER);
+        System.out.println("Pressed Enter in search field.");
+
+        List<WebElement> productNames = driver.findElements(By.cssSelector(".product-name a"));
+
+        System.out.println("Stored " + productNames.size() + " product names from search results.");
+
+        for (WebElement productName : productNames) {
+            assertThat("Some of the product names do not contain the searched keyword.",
+                    productName.getText(), containsString(keyword.toUpperCase())
+            );
+        }
     }
 
     @Test
@@ -32,6 +52,16 @@ public class SimpleSearchTest {
         String oldPrice = driver.findElement(By.xpath("//p[@class='old-price']//span[@class='price']")).getText();
         String specialPrice = driver.findElement(By.xpath("//p[@class='special-price']//span[@class='price']")).getText();
 
+        String oldPriceValue = oldPrice.split(" ")[0]
+                .replace(",", ".");
+
+        String specialPriceValue = specialPrice.split(" ")[0]
+                .replace(",", ".");
+
+//        assertThat("Old price greater than special price",
+//                Double.parseDouble(oldPriceValue),
+//                is(lowerThan(Double.parseDouble(specialPriceValue)))
+//                );
 
     }
 
